33a007ce152482b8b9e2e8c6be71c317f7ad1331
anooshk <anooshk@192.168.0.3>
2016-04-01 00:09:19 +0530

    Guru99 GIT Test Demo First Commit Message

---
+++ gittest/src/test/java/guru99/gittest/AppTest.java
@@ -0,0 +1,35 @@
+package guru99.gittest;
+
+import org.openqa.selenium.WebDriver;
+import org.openqa.selenium.firefox.FirefoxDriver;
+import org.testng.annotations.Test;
+
+public class AppTest 
+{
+	protected WebDriver driver;
+	
+	@Test
+	public void guru99tutorials()
+	{
+		driver = new FirefoxDriver();
+		
+		String eTitle = "Meet Guru99";
+		String aTitle = "";
+		
+		driver.get("http://www.guru99.com");
+		driver.manage().window().maximize();
+		
+		aTitle = driver.getTitle();
+		
+		if (aTitle.contentEquals(eTitle))
+		{
+			System.out.println("Test Passed");
+		}
+		else
+		{
+			System.out.println("Test Failed");
+		}
+		
+		driver.close();
+	}
+}
