149217faba3e1e6368f2897e81a76969dd5d4635
anooshk <anooshk@192.168.0.3>
2016-04-01 20:24:05 +0530

    Added log4.properties file Added Maual.logs file Added Selenium.logs file Modified AppTest.java to trace logs

--- gittest/src/test/java/guru99/gittest/AppTest.java
+++ gittest/src/test/java/guru99/gittest/AppTest.java
@@ -1,5 +1,6 @@
 package guru99.gittest;
 
+import org.apache.log4j.Logger;
 import org.openqa.selenium.WebDriver;
 import org.openqa.selenium.firefox.FirefoxDriver;
 import org.testng.annotations.Test;
@@ -11,23 +12,32 @@ public class AppTest
 	@Test
 	public void guru99tutorials()
 	{
+		Logger log = Logger.getLogger("devpinoyLogger");
+		
 		driver = new FirefoxDriver();
+		log.debug("Firefox Browser Opened");
+		
 		
 		String eTitle = "Meet Guru99";
 		String aTitle = "";
 		
 		driver.get("http://www.guru99.com");
+		log.debug("Guru99 Site Opened");
+		
 		driver.manage().window().maximize();
+		log.debug("Window Maximized");
 		
 		aTitle = driver.getTitle();
-		
+		log.debug("Title of the Window is: "+eTitle);
 		if (aTitle.contentEquals(eTitle))
 		{
 			System.out.println("Test Passed");
+			log.debug("Title matching with expected");
 		}
 		else
 		{
 			System.out.println("Test Failed");
+			log.debug("Title Not matching with expected");
 		}
 		
 		driver.close();
